78168c9a925fb5c5c06ba3c1e7b4dac437827760
Balu9911 <sbalu2401@gmail.com>
2024-08-27 13:36:04 +0530

    tst

---
+++ test.java
@@ -0,0 +1,19 @@
+class A{}
+class B{
+    public static void main(String[] args) {
+        int a=10;
+        a="balu";
+        System.out.println("a");
+    }
+}
+class C{
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a);
+    }
+}
+class test{
+    public static void main(String[] args) {
+        System.out.println("GOOD MORNING");
+    }
+}
---
+++ test1.java
@@ -0,0 +1,6 @@
+class test1{
+ public static void main(String[] args) {
+    int a=10;
+    System.out.println(a);
+ }   
+}
