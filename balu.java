d23d6661f9b606d0b64223f8b6b6a365f23e519b
Balu9911 <sbalu2401@gmail.com>
2024-08-28 13:55:29 +0530

    https://github.com/Balu9911/java.git

---
+++ balu.java
@@ -0,0 +1,23 @@
+class balu{
+    public static void main(String[] args) {
+        byte b=10;
+        short s=100;
+        int price=20000;
+        long mobile=9912460679l;
+        float esal1=23000.55f;
+        double esal2=22000.77;
+        double rating=4.5;
+        char avail='B';
+        boolean discount=true;
+        System.out.println("b");
+        System.out.println("s");
+        System.out.println("price");
+        System.out.println("mobile");
+        System.out.println("esal1");
+        System.out.println("esal2");
+        System.out.println("rating");
+        System.out.println("discount");
+        System.out.println("avail");
+
+    }
+}
